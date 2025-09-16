bda1b02176d520e389fd015a27812e0ef3121e34
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2025-09-16 23:56:50 +0530

    Handle invalid commands: Removed the new line token at the end of fgets

--- src/main.c
+++ src/main.c
@@ -11,6 +11,8 @@ int main(int argc, char *argv[]) {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  size_t input_len = strlen(input);
+  if (input > 0 && input[input_len - 1] == '/n]') input[input_len - 1] = '\0';
 
   // Print the input
   printf("%s: command not found\n", input);
